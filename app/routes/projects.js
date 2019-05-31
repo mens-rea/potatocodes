488f15f082ba5da0c9cfd4062af41e4cd8fb8015
Juan Miguel Alvarez <juan.miguel.g.alvarez@gmail.com>
2019-05-31 10:13:52 +0700

    Add link to card list

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -3,9 +3,9 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return [
-      {name: 'Wanderast', img: 'assets/images/projects/wanderast.png'}, 
-      {name: 'Offcrowd', img: 'assets/images/projects/offcrowd.png'},
-      {name: 'Liham PH', img: 'assets/images/projects/liham.png'}
+      {name: 'Wanderast', img: 'assets/images/projects/wanderast.png', link: 'https://wanderast.com'}, 
+      {name: 'Offcrowd', img: 'assets/images/projects/offcrowd.png', link: 'https://offcrowd.com'},
+      {name: 'Liham PH', img: 'assets/images/projects/liham.png', link: 'https://liham.ph'}
     ];
   }
 });
